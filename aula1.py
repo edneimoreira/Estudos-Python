3a8c50bdbb1799292d0ac84f68aecfb4512562ce
edneimoreira <edsantana0@gmail.com>
2021-09-13 20:39:09 -0300

    primeiro commit

---
+++ aula1.py
@@ -0,0 +1,26 @@
+imped_idade =''
+imped_peso =''
+imped_bebida =''
+imped_tatuagem =''
+idade = int (input('Digite a idade:'))
+peso = int (input('Digite o peso:'))
+bebida = int (input('Ingeriu bebida alcolica nas ultimas 12hs? Digite: 1- Para sim ou 2- Para não: '))
+tatuagem = int (input('Fez alguma tatuagem no ultimo ano? Digite: 1- Para sim ou 2- Para não: '))
+
+if idade < 19 or idade > 69:
+    imped_idade = "Idade fora da faixa de doação."
+
+if peso < 50:
+    imped_peso = 'Peso inferior a 50kg.'
+
+if bebida ==1:
+    imped_bebida ='Ingestão de bebida alcoólica.'
+
+if tatuagem ==1:
+    imped_tatuagem ="Tatuagem recente."        
+
+if (imped_idade !='') or (imped_peso !='') or (imped_bebida !='') or (imped_tatuagem !='') :
+    print ('Não pode doar, motivo: ' + imped_idade + " " + imped_peso + " " + imped_bebida + " " + imped_tatuagem)
+
+else: 
+    print ('Pode doar sangue')
